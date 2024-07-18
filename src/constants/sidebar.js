84de6cb15fb5ae4330dd1b50496ee2050e643f71
Shahriyor <94789479+shahriyorreal@users.noreply.github.com>
2024-07-18 19:28:02 +0500

    Menu

--- src/constants/sidebar.js
+++ src/constants/sidebar.js
@@ -1,3 +1,5 @@
+
+
 export default [
     {
         label: 'Дашборд',
@@ -9,35 +11,272 @@ export default [
         items: [
             {
                 label: 'Поступление товаров',
-                icon: 'pi pi-eraser',
+                icon: 'pi pi-palette',
                 route: '/purchase'
             },
             {
                 label: 'Заявка товаров',
-                icon: 'pi pi-heart',
-                route: ''
-            }
-        ]
-    },
-    {
-        label: 'Programmatic',
-        icon: 'pi pi-link',
-    },
-    {
-        label: 'External',
-        icon: 'pi pi-home',
-        items: [
-            {
-                label: 'Vue.js',
-                icon: 'pi pi-star',
-                url: 'https://vuejs.org/'
+                icon: 'pi pi-eraser',
+                route: '/www'
             },
             {
-                label: 'Vite.js',
-                icon: 'pi pi-bookmark',
-                url: 'https://vuejs.org/'
+                label: 'Возврат товаров',
+                icon: 'pi pi-eraser',
+                route: '/eee'
+            },
+            {
+                label: 'Заявка товаров',
+                icon: 'pi pi-heart',
+                route: '/ooo'
             }
         ]
-    }
+    },
+    {
+        label: 'Продажа',
+        icon: 'pi pi-link',
+        items: [
+            {
+                label: 'Продажа клиентам',
+                route: '/client'
+            },
+            {
+                label: 'Возврат от клиентов',
+                route: '/client'
+            },
+            {
+                label: 'Заказ клиентов',
+                route: '/client'
+            }
+        ]
+    },
+    {
+        label: 'Товары',
+        icon: 'pi pi-link',
+        items: [
+            {
+                label: 'Продажа товары',
+                route: '/client'
+            },
+            {
+                label: 'Возврат товаров',
+                route: '/client'
+            },
+            {
+                label: 'Заказ товары',
+                route: '/client'
+            }
+        ]
+    },
+    {
+        label: 'Финансы',
+        icon: 'pi pi-link',
+        items: [
+            {
+                label: 'Приход средств',
+                route: '/client'
+            },
+            {
+                label: 'Расход средств',
+                route: '/client'
+            },
+            {
+                label: 'Приход рас.сч',
+                route: '/client'
+            },
+            {
+                label: 'Расход рас.сч',
+                route: '/client'
+            }
+        ]
+    },
+    {
+        label: 'Склады',
+        icon: 'pi pi-link',
+        items: [
+            {
+                label: 'Отприходование',
+                route: '/client'
+            },
+            {
+                label: 'Списание',
+                route: '/client'
+            },
+            {
+                label: 'Инвентаризация',
+                route: '/client'
+            },
+        ]
+    },
+    {
+        label: 'Справочники',
+        icon: 'pi pi-link',
+        items: [
+            {
+                label: 'Единицы измериния',
+                route: '/client'
+            },
+            {
+                label: 'Пользователи',
+                route: '/client'
+            },
+            {
+                label: 'Контрагенты',
+                route: '/client'
+            },
+            {
+                label: 'Мои организации',
+                route: '/client'
+            },
+            {
+                label: 'Виды цен',
+                route: '/client'
+            },
+            {
+                label: 'Волюты',
+                route: '/client'
+            }
+        ]
+    },
+    {
+        label: 'Планирование',
+        icon: 'pi pi-link',
+        items: [
+            {
+                label: 'Товар',
+                route: '/client'
+            },
+            {
+                label: 'Магазины',
+                route: '/client'
+            },
+            {
+                label: 'Склады',
+                route: '/client'
+            }
+        ]
+    },
+    {
+        label: 'Отчёты',
+        icon: 'pi pi-link',
+        items: [
+            {
+                label: 'Взаиморасчет с поставщиками',
+                route: '/client'
+            },
+            {
+                label: 'Взаиморасчет с клиентами',
+                route: '/client'
+            },
+            {
+                label: 'Акт сверки с поставщиками',
+                route: '/client'
+            },
+            {
+                label: 'Акт сверки с клиентами',
+                route: '/client'
+            },
+            {
+                label: 'Отчет покупки поставщиков',
+                route: '/client'
+            },
+            {
+                label: 'Отчет покупки клиентов',
+                route: '/client'
+            },
+            {
+                label: 'АБС анализ',
+                route: '/client'
+            },
+        ]
+    },
+    {
+        label: 'Кадры',
+        icon: 'pi pi-link',
+        items: [
+            {
+                label: 'Сотрудники',
+                route: '/client'
+            },
+            {
+                label: 'Должности',
+                route: '/client'
+            },
+            {
+                label: 'Прием на работу',
+                route: '/client'
+            },
+            {
+                label: 'Кадровое перемещение',
+                route: '/client'
+            },
+            {
+                label: 'Увольнение',
+                route: '/client'
+            },
+            {
+                label: 'Начисление зарплаты',
+                route: '/client'
+            },
+            {
+                label: 'Оплата зарплаты',
+                route: '/client'
+            },
+            {
+                label: 'Табель',
+                route: '/client'
+            },
+            {
+                label: 'Ведомость зарплаты',
+                route: '/client'
+            },
+        ]
+    },
+    {
+        label: 'CRM',
+        icon: 'pi pi-link',
+        items: [
+            {
+                label: 'Установка Цен',
+                route: '/client'
+            },
+            {
+                label: 'Установка скидок',
+                route: '/client'
+            },
+            {
+                label: 'Смс Рассылка',
+                route: '/client'
+            },
+            {
+                label: 'Дисконтные карты',
+                route: '/client'
+            },
+
+        ]
+    },
+    {
+        label: 'Настройки',
+        icon: 'pi pi-link',
+        items: [
+            {
+                label: 'Настройка',
+                route: '/client'
+            },
+            {
+                label: 'Ввод начальных графика',
+                route: '/client'
+            },
+            {
+                label: 'Погашения',
+                route: '/client'
+            },
+            {
+                label: 'Ввод нач. остаток',
+                route: '/client'
+            },
+
+        ]
+    },
+
 
 ]
