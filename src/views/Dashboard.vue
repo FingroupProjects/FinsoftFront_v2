dba2ced4abdeac01292f0c69f564405f7bd1f11f
jaxongirxon <jaxongirxon.rustamov12@gmail.com>
2024-08-07 16:57:11 +0500

    bug fix

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -31,6 +31,7 @@ import OverlayBadge from 'primevue/overlaybadge';
             </OverlayBadge>
             <div>
 
+
               <button type="button"
                       class="flex text-sm bg-gray-800 rounded-full focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
                       aria-expanded="false" data-dropdown-toggle="dropdown-user">
