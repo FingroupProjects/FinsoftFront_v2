5a6890f11ac934313dc8eaae8b117a4e76dec7b8
user <jaxongirxon.rustamov12@gmail.com>
2024-10-12 11:27:53 +0500

    add page price list

--- src/components/priceSetting/CardGoods.vue
+++ src/components/priceSetting/CardGoods.vue
@@ -2,22 +2,25 @@
 import {watchEffect, ref, onMounted, watch} from 'vue';
 import formatPrice from '@/constants/formatNumber.js';
 
-const emit = defineEmits(['delete']);
+const emit = defineEmits(['delete','scrollPosition']);
 const props = defineProps({
   infoList: Object,
   priceList: Array,
   priceTypeId: Number,
   scrollPosition: Number,
-
 });
 
 const scrollTop = ref(null);
-const inputValues = ref(Array(props.priceList.length).fill(Number(props.infoList.newPrice)));
+const inputValues = ref();
 const allOldPrice = ref(0);
 const newAllPrice = ref(0);
 
 const isInputGreater = (index) => {
   return inputValues.value[index] > Number(props.infoList.oldPrice);
+};
+
+const syncScroll = (event) => {
+  emit('scrollPosition',event.target.scrollTop)
 };
 
 onMounted(() => {
@@ -33,6 +36,7 @@ watch(() => props.scrollPosition, (newPosition) => {
 });
 
 watchEffect(() => {
+  inputValues.value=Array(props.priceList.length).fill(Number(props.infoList.newPrice))
   allOldPrice.value = props.priceList[0].reduce((total, item) => {
     return total + Number(item.oldPrice);
   }, 0);
@@ -41,11 +45,10 @@ watchEffect(() => {
   }, 0);
   scrollTop.value
 });
-
 </script>
 
 <template>
-  <div class="rounded-[10px] bg-[#fff] px-4 py-4 h-[80vh] w-[17%] overflow-y-scroll div_big" ref="scrollTop">
+  <div @scroll="syncScroll" class="rounded-[10px] bg-[#fff]  h-[80vh] w-[17%] overflow-y-scroll div_big" ref="scrollTop">
     <div class="header flex gap-[13px] items-center">
       <div class="text-[600] text-[16px] leading-[16px] text-[#000] truncate ">
         {{ props.infoList.priceType }}
@@ -55,7 +58,7 @@ watchEffect(() => {
       </button>
     </div>
     <div v-for="(item,index) in props.priceList" :key="index"
-         class="content mt-[19px] flex gap-5 items-center overflow-y-scroll">
+         class="content  flex gap-5 items-center">
       <div class="text-[15px] font-medium leading-[15px] text-[#141C30]">
         {{ item.oldPrice || 0 }}
       </div>
@@ -65,7 +68,7 @@ watchEffect(() => {
         <img v-else class="absolute top-[10px] right-[10px]" src="@/assets/img/arrowDanger.svg" alt="">
       </div>
     </div>
-    <div class="flex p-3 justify-between fixed z-10 bg-white w-[190px] bottom-0" style="border-top:1px solid #DCDFE3">
+    <div class="flex p-3 justify-between fixed z-10 bg-white w-[215px] bottom-0 rounded-b-[10px]" style="border-top:1px solid #DCDFE3">
       <div class="text-[#141C30] font-semibold text-[15px] leading-[15px]">
         {{ formatPrice(allOldPrice) }}
         <div class="text-[#808BA0] font-semibold text-[11px] leading-[11px] text-center">
@@ -85,13 +88,21 @@ watchEffect(() => {
 <style scoped lang="scss">
 .header {
   border-bottom: 1px solid #DCDFE3;
-  padding-bottom: 20px;
 }
-
+.content{
+  padding: 0.75rem 1rem 0.75rem 1rem;
+  //border-style: solid;
+  //border-width: 0 0 1px 0;
+  height: 40px;
+}
 .truncate {
   overflow: hidden;
   white-space: nowrap;
   text-overflow: ellipsis;
+  padding: 0.75rem 1rem 0.75rem 1rem;
+  height: 64px;
+  display: flex;
+  align-items: center;
 }
 
 .text-container {
